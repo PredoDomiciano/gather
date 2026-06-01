091bfc21d80c41b5ba775cab86c80b6055c2ff21
unknown <diogoteodoro2014@Outlook.com>
2026-06-01 18:54:23 -0300

    fix: android

--- gather/android/app/build.gradle.kts
+++ gather/android/app/build.gradle.kts
@@ -20,10 +20,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.gather"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        // CORRIGIDO: Agora o applicationId bate exatamente com o namespace e a sua MainActivity
+        applicationId = "com.BTTech.gather"
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,7 +32,7 @@ android {
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now, so flutter run --release works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
---
+++ gather/android/app/src/main/kotlin/com/BTTech/gather/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.BTTech.gather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
